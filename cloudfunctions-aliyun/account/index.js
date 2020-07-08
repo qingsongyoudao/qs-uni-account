bade74042eff53f9312c695b1b7d439223a47e27
qingsongyoudao <dawuhan08@163.com>
2020-07-08 16:48:49 +0800

    update

--- cloudfunctions-aliyun/account/index.js
+++ cloudfunctions-aliyun/account/index.js
@@ -27,7 +27,49 @@ exports.main = async (event, context) => {
 			res = await qsAccount.loginByPwd(params);
 			break;
 		case 'login-sms':
-			res = await qsAccount.loginBySms()(params);
+			res = await qsAccount.loginBySms(params);
+			break;
+		case 'login-weixin':
+			res = await qsAccount.loginByWeixin(params);
+			break;
+		case 'logout':
+			res = await qsAccount.logout(params);
+			break;
+		case 'set-password':
+			res = await qsAccount.setPassword(params);
+			break;
+		case 'update-password':
+			res = await qsAccount.updatePassword(params);
+			break;
+		case 'set-userName':
+			res = await qsAccount.setUserName(params);
+			break;
+		case 'update-userName':
+			res = await qsAccount.updateUserName(params);
+			break;
+		case 'bind-email':
+			res = await qsAccount.bindEmail(params);
+			break;
+		case 'update-email':
+			res = await qsAccount.updateEmail(params);
+			break;
+		case 'unbind-email':
+			res = await qsAccount.unbindEmail(params);
+			break;
+		case 'bind-mobile':
+			res = await qsAccount.bindMobile(params);
+			break;
+		case 'update-mobile':
+			res = await qsAccount.updateMobile(params);
+			break;
+		case 'unbind-mobile':
+			res = await qsAccount.unbindMobile(params);
+			break;
+		case 'set-avatar':
+			res = await qsAccount.setAvatar(params);
+			break;
+		case 'update-user':
+			res = await qsAccount.updateUser(params);
 			break;
 		default:
 			res = {
