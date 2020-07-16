544ae2a83609d21b9516af60a87318cf2c244a65
qingsongyoudao <dawuhan08@163.com>
2020-07-16 21:43:43 +0800

    update

--- pages/account/login/sms-login.vue
+++ pages/account/login/sms-login.vue
@@ -43,6 +43,7 @@ export default {
 			title: '短信验证码登录',
 			desc: '',
 			codeTips: '',
+			codeId: '',
 			form: {
 				button: {
 					loading: false
@@ -52,7 +53,9 @@ export default {
 				/* 手机号 */
 				mobile: '',
 				/* 验证码 */
-				verifyCode: ''
+				verifyCode: '',
+				/* 验证码编号 */
+				codeId: ''
 			},
 			rules: {
 				mobile: [
@@ -96,6 +99,7 @@ export default {
 			});
 		},
 		submit() {
+			this.model.codeId = this.codeId
 			this.$refs.uForm.validate(valid => {
 				if (valid) {
 					this.form.button.loading = true;
@@ -134,15 +138,16 @@ export default {
 					title: '正在获取验证码',
 					mask: true
 				});
+				this.codeId = this.$u.random(100000, 999999).toString();
 				setTimeout(() => {
 					uni.hideLoading();
 					// 这里此提示会被this.start()方法中的提示覆盖
-					this.$u.toast('验证码已发送');
+					this.$u.toast(this.codeId + '验证码已发送');
 					// 通知验证码组件内部开始倒计时
 					this.$refs.uCode.start();
 				}, 2000);
 			} else {
-				this.$u.toast('倒计时结束后再发送');
+				this.$u.toast(this.codeId + '倒计时结束后再发送');
 			}
 		}
 	},
